3f8ffc5c01e5a9bc4116f4d9a18c02db2c06f3a9
Knightly Rhythms <aksinghjunior@gmail.com>
2026-02-23 14:19:22 +0530

    being safe

---
+++ scripts/powermenu.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# List of options
+options="Lock\nSuspend\nLog out\nReboot\nShutdown"
+
+# Run fuzzel
+chosen=$(echo -e "$options" | fuzzel --dmenu -p "Power Menu: ")
+
+# Act on selection
+case "$chosen" in
+    *Lock) swaylock ;; # Or hyprlock
+    *Suspend) systemctl suspend ;;
+    *"Log out") swaymsg exit ;; # Or hyprctl dispatch exit
+    *Reboot) systemctl reboot ;;
+    *Shutdown) systemctl poweroff ;;
+esac
+
